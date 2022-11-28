369eaee239585b06abd160eaff47d1a058163427
Turjo-abhishek <turjoabhishek@gmail.com>
2022-11-28 16:28:37 +0600

    added stripe and added a orders route for particular order

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get("/orders/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await bookingsCollection.findOne(query);
+            res.send(result);
+        })
+
 
         // Users
 
